f34bc90ca7d5ffa690f7d9b8d784a5b92bcf7f1d
thantnaylin <thantnaylin1301@gmail.com>
2023-05-29 23:07:30 +0800

    Add post model, controller and routes

---
+++ server/controllers/posts.js
@@ -0,0 +1,64 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+import Posts from "../routes/posts.js";
+
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+    const post = await Post.find(); // Naughty naughty way to do things
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+export const getFeedPosts = async (req, res) => {
+  try {
+    const posts = await Post.find(); // Naughty naughty way to do things
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const posts = await Post.find({ userId }); // Naughty naughty way to do things
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      posts.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(id, { likes: post.likes });
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
